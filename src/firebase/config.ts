658b395da7ebb73edb6f266fd79fe2aee249c5ed
empresabizly <empresabizly@gmail.com>
2026-07-28 16:07:57 -0600

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,3 +1,8 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+
 const firebaseConfig = {
   apiKey: 'AIzaSyCM2z5slUjr8vyEPwASzT7pArS0unQyYvE',
   authDomain: 'bizly-store.firebaseapp.com',
@@ -6,3 +11,10 @@ const firebaseConfig = {
   messagingSenderId: '717745007360',
   appId: '1:717745007360:web:eb81dfcae6b08397f761e5',
 };
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+export default app;
